bf1e5646a1447adc8f45d83f47aa6a39db8fb656
sravanthigalla933 <sravanthigalla933@gmail.com>
2024-09-04 08:11:49 +0530

    deploy_script

---
+++ deploy3.py
@@ -0,0 +1,29 @@
+#!//u01/app/oracle/middleware/wls12c/oracle_common/common/bin/wlst.sh
+def wlDeployUndeploy(username, password, adminURL, appName, location, targets):
+    try:
+        #connect to admin server
+        connect(username, password, adminURL)
+        #start edit operation
+        edit()
+        startEdit()
+
+        #stop application
+        stopApplication(appName)
+
+        #start undeploying application to "AdminServer" server
+        progress = undeploy(appName, timeout=60000)
+        progress.printStatus()
+        save()
+        activate(20000,block="true")
+        #start deploying application to ""AdminServer" server
+        progress = deploy(appName,location,targets)
+        progress.printStatus()
+        #print 'Done deploying application' +appname
+
+    except Exception, e:
+         print ex.toString()
+wlDeployUndeploy('weblogic','weblogic1','t3://192.168.56.101:7001','benefits','/u01/app/oracle/middleware/applications/benefits.war','Cluster-1')
+wlDeployUndeploy('weblogic','weblogic1','t3://192.168.56.101:7001','messaging','/u01/app/oracle/middleware/applications/messaging.war','Cluster-1')
+wlDeployUndeploy('weblogic','weblogic1','t3://192.168.56.101:7001','contacts','/u01/app/oracle/middleware/applications/contacts.war','Cluster-1')
+wlDeployUndeploy('weblogic','weblogic1','t3://192.168.56.101:7001','timeoff','/u01/app/oracle/middleware/applications/timeoff.war','Cluster-1')
+
